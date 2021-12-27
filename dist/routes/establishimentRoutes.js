42026a31e286303029649552c860fb4935fe0f97
LourencoLima <Lourenco Testa Pereira Lima>
2021-12-27 02:02:34 -0300

    Config inicial

--- dist/routes/establishimentRoutes.js
+++
@@ -1,9 +0,0 @@
-"use strict";Object.defineProperty(exports, "__esModule", {value: true});var _express = require('express');
-
-const router = _express.Router.call(void 0, );
-
-router.get('/', (req, res) => {
-  return res.status(200).json({ message: 'Olá!' });
-});
-
-exports. default = router;
