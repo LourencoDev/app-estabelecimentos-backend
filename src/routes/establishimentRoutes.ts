4e861b444193a3789a16b65e311d147e12ddac5b
LourencoLima <Lourenco Testa Pereira Lima>
2021-12-27 02:03:46 -0300

    Config inicial

--- src/routes/establishimentRoutes.ts
+++ src/routes/establishimentRoutes.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from "express";
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  return res.status(200).json({ message: 'Olá!' });
+  return res.status(200).json({ message: 'Olá!!!' });
 });
 
 export default router;
