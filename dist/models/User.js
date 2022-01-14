2240fe1d9d8f725f078db056a8dda51a4184ffeb
LourencoLima <Lourenco Testa Pereira Lima>
2022-01-14 00:42:50 -0300

    dist

---
+++ dist/models/User.js
@@ -0,0 +1,23 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true});
+
+@Entity('user')
+ class User {
+  Generated('uuid')
+  @PrimaryColumn('int')
+  
+  
+  Column()
+  
+  
+  Column()
+  
+  
+  CreateDateColumn()
+  
+  
+  UpdateDateColumn()
+  
+
+  DeleteDateColumn()
+  
+} exports.User = User;
---
+++ dist/routes/authRoutes.js
@@ -0,0 +1,13 @@
+"use strict";Object.defineProperty(exports, "__esModule", {value: true}); function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }var _express = require('express');
+var _AuthController = require('../controllers/AuthController');
+var _authorize = require('../middlewares/authorize'); var _authorize2 = _interopRequireDefault(_authorize);
+
+const router = _express.Router.call(void 0, );
+router.post('/register', _AuthController.AuthController.register);
+router.post('/login', _AuthController.AuthController.login);
+router.post('/', _authorize2.default.authenticate, _AuthController.AuthController.home);
+router.get('/', (req, res) => {
+  return res.status(200).json({ message: 'OK' });
+});
+
+exports. default = router;
