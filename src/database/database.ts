8520f838c82ad3565c2453697db1c9d4275f9e50
Lourenço Lima <51246899+LourencoDev@users.noreply.github.com>
2022-07-02 01:01:14 -0300

    Update database.ts

--- src/database/database.ts
+++ src/database/database.ts
@@ -6,7 +6,6 @@ export default class Database {
   public static async connection() {
     try {
       const config = constants.IS_PROD ? dbconfig.OrmConfigProd : dbconfig.OrmConfigDev as any;
-      console.log(config);
       await createConnection(config);
       console.log('Conexão com o Banco de Dados estabelecida com sucesso!');
     } catch(err) {
